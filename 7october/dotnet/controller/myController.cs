d148ca83626ab4792425cf9ae9ebfd623aa9f5ab
BM-41756Harishma <harishmahari1502@gmail.com>
2016-10-08 11:07:12 +0530

    sprint3

---
+++ 7october/dotnet/controller/myController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication8.Models;
+using System.Web.UI.WebControls;
+using System.IO;
+
+
+namespace MvcApplication8.Controllers
+{
+    public class myController : Controller
+    {
+        //
+        // GET: /my/
+        //[AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+       [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Index(ModelClass1 mc, string cmd)
+        {
+           
+           if (cmd == "Login")
+           {
+               string msg = mc.login();
+               if(msg.Contains("Responce code:200"))
+                {
+                   return View("Homepage");
+               }
+               return Content("<script language='javascript'type='text/javascript'>alert('" + msg + "');</script>");
+               //return View(mc);
+               
+           }
+
+
+           if (cmd == "create an account")
+           {
+
+               //var today = new Date();
+               int currentYear = Convert.ToInt32(DateTime.Now.Year.ToString());
+
+               int year = Convert.ToInt32(mc.X_year);
+               int age = currentYear - year;
+               if (age >= 13)
+               {
+                   
+                  FileUpload fld1 = new FileUpload();
+                  string profpictr = "F:\\proflpict\\" + mc.proflepic;
+                  fld1.SaveAs("F:\\proflpict\\" + mc.proflepic);
+                   //long filesize = new FileInfo(profpictr).Length;
+                   FileInfo fileInfo = new FileInfo("F:\\proflpict\\"+mc.proflepic);
+                   double fileSizeKB = fileInfo.FullName.Length / 1024;
+                   double fileSizeMB = fileInfo.FullName.Length / (1024 * 1024);
+                  /* long fileSizeInBytes = fld1.FileContent.Length;
+                   // Convert the bytes to Kilobytes (1 KB = 1024 Bytes)
+                   long fileSizeInKB = fileSizeInBytes / 1024;
+                   // Convert the KB to MegaBytes (1 MB = 1024 KBytes)
+                   long fileSizeInMB = fileSizeInKB / 1024;*/
+                  // if (filesize < 5)
+                   //{
+                        string msgg = mc.signup();
+                       // return Content("<script language='javascript'type='text/javascript'>alert('" + msgg + "');</script>");
+                        return View("Vpage");
+                   //}
+               }
+              /* else
+               {
+                   return null;
+               }*/
+
+               
+              }
+           return null;
+           }
+    }
+
+ }
---
+++ 7october/dotnet/view/Homepage.aspx
@@ -0,0 +1,33 @@
+﻿
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+   <link href="../../Content/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="../../Content/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+ </head>
+ <body>
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-md-12" style="background-color:#3b5998; height:40px;"> 
+    <button type="button" class="btn btn-default" style="margin-left:50px; margin-top:5px; width:150px;">
+      <span class="glyphicon glyphicon-search"></span>Search Facebook
+    </button>
+    <img src="../../Content/imgs/rose1.png" style="margin-left:400px; margin-top:5px; width:30px;"/>
+    <input type="submit" value="Haris" class="btn-primary" /></div>
+    </div>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+    <div class="col-md-4" style="background-color:Silver;width:200px;height:523px;"><img src="../../Content/imgs/rose1.png" style="width:20px;" />Haris Np <br />Edit profile</div>
+    <div class="col-md-7" style="background-color:Silver; width:450px;"><pre><a href="#">Update Status</a>&nbsp;&nbsp;&nbsp;&nbsp;  <a href="#">Add photos/video</a>&nbsp;&nbsp;&nbsp;&nbsp;  <a href="#">Writenotes</a><br />
+   <img src="../../Content/imgs/rose1.png" style="width:20px" /> What's in your mind? </pre><br /><pre style="height:400px;"></pre></div>
+    <div class="col-md-1" style="background-color:Silver; height:523px; width:400px;"><pre style="width:400px;">Your adds<div style="background-color:Blue;width:300px;height:475px; margin-left:50px;"></pre></div></div>
+    </div>
+</body>
+</html>
